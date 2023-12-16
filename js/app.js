b073036bf9a3a611744148873acb1f817b7adb1e
Bruno cordoba <brunonahuel87@gmail.com>
2023-12-16 17:14:06 -0300

    cree arreglo con las peliculas

--- js/app.js
+++ js/app.js
@@ -3,9 +3,25 @@ import { Pelicula } from "./classPelicula.js";
 const formulario = document.querySelector('form');
 console.log(formulario);
 
+//datos formulario
+let nombre = document.getElementById('nombre')  ;
+let categoria = document.getElementById('categoria')  ;
+let descripcion= document.getElementById('descripcion')  ;
+let publicado= document.getElementById('subidoPagina')  ;
+
+const peliculas = [];
+
+
 function crearContacto(e) {
     e.preventDefault()
     console.log("Desde funcion");   
+
+    
+
+const pelicula = new Pelicula(nombre.value , categoria.value , publicado.value ,descripcion.value);
+
+peliculas.push(pelicula);
+console.log(peliculas)
 }
 
 formulario.addEventListener('submit',crearContacto);
